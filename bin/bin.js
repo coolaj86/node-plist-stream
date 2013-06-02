d4e17634ea6734a8a3a06f0a1b5b62487894b65d
AJ ONeal <coolaj86@gmail.com>
2013-06-02 05:36:36 -0500

    started migrating events to bin

--- bin/bin.js
+++ bin/bin.js
@@ -2,9 +2,78 @@
   "use strict";
 
   var fs = require('fs')
-    , plist = require('../lib/index.js').create()
+    , Plist = require('../lib/index.js')
     , pathname = process.argv[2]
+    , util = require('util')
     ;
 
-  plist.parse(fs.createReadStream(pathname));
+  function peek(n) {
+    /*jshint validthis:true*/
+    var me = this
+      ;
+
+    n = n || 0;
+
+    return me[(me.length - 1) + n];
+  }
+
+  function run() {
+    var plist = Plist.create()
+      , keyStack = []
+      , valStack = []
+      ;
+
+    valStack.peek = peek;
+    keyStack.peek = peek;
+
+    plist = plist.parse(fs.createReadStream(pathname));
+
+    // Object cycle
+    plist.on('pushObj', function () {
+      util.print('{');
+    });
+    plist.on('pushKey', function (key) {
+      util.print('\n"' + key.replace(/"/g, '\\"') + '": ');
+    });
+    plist.on('assignVal', function (val) {
+    });
+    plist.on('popKey', function () {
+      // awkward
+      util.print(',');
+    });
+    plist.on('popObj', function () {
+      util.print('}');
+    });
+
+    // Array cycle
+    plist.on('pushArray', function () {
+      util.print('\n[');
+    });
+    plist.on('pushIndex', function () {
+    });
+    plist.on('pushEl', function (el) {
+    });
+    plist.on('popIndex', function () {
+      util.print(',');
+    });
+    plist.on('popArray', function () {
+      util.print(']');
+    });
+
+    // any value cycle
+    plist.on('pushVal', function (val) {
+      valStack.push(val);
+    });
+    plist.on('popVal', function () {
+      valStack.pop();
+    });
+
+    // end of stream
+    plist.on('end', function () {
+      console.log('\n\nFIN');
+      console.log(JSON.stringify(valStack[0], null, '  '));
+    });
+  }
+
+  run();
 }());
